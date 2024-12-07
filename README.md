3382083fa2def05ecb8df43edb72f4bc717a5d03
Julia Andrews <theunderdog09@gmail.com>
2024-12-07 15:47:08 -0500

    Allow declaring variables in terms of same-name variables
    There's some funkiness re: variables in block scopes not being properly
    captured by closures. There's a currently-failing test to catch this
    behavior (tests/eval/shadowing.lx@shadowing_in_block), and I need to
    tweak the environment implementation to resolve it.

--- README.md
+++ README.md
@@ -1,11 +1,66 @@
 Following along (loosely) with [_Crafting Interpreters_][CI] in Rust.
 
-Going a lot harder on several bits of the design than the book does. In particular, aiming
-for much more robust source code representations and error handling/reporting.
+Going a lot harder on several bits of the design than the book does. In
+particular, aiming for much more robust source code representations and error
+handling/reporting.
 
-I'm currently early on in part 1 of the book. The tree-walking interpreter will live in
-[`tlox`](./tlox). When I get to part two (the bytecode interpreter), many components of
-that crate (e.g. source maps, error handling, lexical and syntactic structures) will be
-separated out into other crates to be shared between the two implementations.
+I'm currently in part 1 of the book. The tree-walking interpreter will live
+in [`tlox`](./tlox). When I get to part two (the bytecode interpreter),
+many components of that crate (e.g. source maps, error handling, lexical and
+syntactic structures) will be separated out into other crates to be shared
+between the two implementations.
+
+# Digressions from the Lox language as specified in the book
+
+I have committed the cardinal sin of diverging from the language spec. In my
+defense, no one will ever use this interpreter.
+
+## Use of shadowed variables in initializers
+
+The book's Lox allows this:
+
+```
+var a = 4;
+var a = a + 5; // a is set to 9
+```
+
+but disallows this:
+
+```
+{
+  var a = 4;
+  var a = a + 5; // Error: cannot reference `a` in its own initializer
+}
+```
+
+This is displeasing to me for two reasons:
+
+- It's an arbitrary inconsistency between the treatment of global vs. local
+  variables. Code in the global scope is special-cased in several cases,
+  to facilitate use of the language in an interactive prompt, but minimizing
+  these discrepancies seems desirable, and this discrepancy in particular seems
+  especially arbitrary.
+- Forbidding references within variable initializers to variables of the
+  same name is counter to my intuition of both how variable declaration and
+  initialization should work and to how variable shadowing should work. The
+  variable should not exist until its initial value is evaluated, and thus any
+  previously-declared variables of the same name should be available for use
+  within that initial value.
+
+Granted, in a dynamically typed language, variable shadowing is of marginal
+use and is _mostly_ equivalent to reassigning a variable (though lexical
+scoping rules do make shadowing useful in certain circumstances even in this
+setting). The no-shadowed-variables-in-initializers rule would also mirror the
+way _function_ declarations operate (the function is declared and exists to be
+referenced recursively within the body of the function), but I think there is
+a difference in kind here that justifies the discrepancy; variable initializers
+are evaluated _immediately and eagerly,_ while function bodies are evaluated
+_lazily when called,_ which makes recursive functions both sensical and useful,
+while recursive variables are nonsensical, and the syntax that would produce
+one is more usefully construed to do something else so long as a natural
+"something else" exists.
+
+So, this version of Lox allows the use of same-name variables in initializers,
+in all cases, including in local scopes.
 
 [CI]: https://craftinginterpreters.com/
--- tlox/src/resolve.rs
+++ tlox/src/resolve.rs
@@ -1,9 +1,8 @@
 //! Variable resolution.
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-use crate::diag::{Diag, DiagKind, Diagnostic};
-use crate::span::{Span, Spanned};
+use crate::span::Spanned;
 use crate::symbol::Symbol;
 use crate::syn::{Expr, ExprNode, Place, Stmt};
 
@@ -76,21 +75,20 @@ impl<'s> Resolver<'s, '_> {
                 self.env.pop_scope();
             }
             Stmt::FunDecl { name, args, body } => {
-                self.env.declare_and_define(*name);
+                self.env.declare(name.node);
 
                 self.env.push_scope();
                 for arg in args {
-                    self.env.declare_and_define(*arg);
+                    self.env.declare(arg.node);
                 }
                 self.resolve_stmts(body);
                 self.env.pop_scope();
             }
             Stmt::Decl { name, init } => {
-                self.env.declare(*name);
                 if let Some(expr) = init.as_ref() {
                     self.resolve_expr(expr.as_ref());
                 }
-                self.env.define(name.node);
+                self.env.declare(name.node);
             }
         }
     }
@@ -126,21 +124,15 @@ impl<'s> Resolver<'s, '_> {
     }
 
     fn resolve_name(&mut self, name: Spanned<Symbol<'s>>) {
-        if let Some(res) = self.env.resolve(name) {
+        if let Some(res) = self.env.resolve(name.node) {
             self.table.references.insert(name, res);
         }
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum VarStatus {
-    Declared,
-    Defined,
-}
-
 #[derive(Default)]
 struct ResolverEnv<'s> {
-    scopes: Vec<HashMap<Symbol<'s>, (Span, VarStatus)>>,
+    scopes: Vec<HashSet<Symbol<'s>>>,
 }
 
 impl<'s> ResolverEnv<'s> {
@@ -148,78 +140,32 @@ impl<'s> ResolverEnv<'s> {
         Self::default()
     }
 
-    fn declare(&mut self, name: Spanned<Symbol<'s>>) {
+    fn declare(&mut self, name: Symbol<'s>) {
         self.scopes
             .last_mut()
-            .map(|scope| scope.insert(name.node, (name.span, VarStatus::Declared)));
-    }
-
-    fn define(&mut self, name: Symbol<'s>) {
-        self.scopes
-            .last_mut()
-            .map(|scope| scope.get_mut(&name).unwrap().1 = VarStatus::Defined);
-    }
-
-    fn declare_and_define(&mut self, name: Spanned<Symbol<'s>>) {
-        self.scopes
-            .last_mut()
-            .map(|scope| scope.insert(name.node, (name.span, VarStatus::Defined)));
+            .map(|scope| scope.insert(name));
     }
 
     fn push_scope(&mut self) {
-        self.scopes.push(HashMap::new());
+        self.scopes.push(HashSet::new());
     }
 
     fn pop_scope(&mut self) {
         self.scopes.pop();
     }
 
-    fn resolve(&mut self, name: Spanned<Symbol<'s>>) -> Option<usize> {
+    fn resolve(&mut self, name: Symbol<'s>) -> Option<usize> {
         self.scopes
             .iter()
             .rev()
             .enumerate()
             .filter_map(|(i, scope)| {
-                let (decl, status) = scope.get(&name.node)?;
-                if *status == VarStatus::Declared {
-                    ResolverDiag::RecursiveVarDecl {
-                        name: name.node,
-                        decl: *decl,
-                        reference: name.span,
-                    }
-                    .emit();
+                if scope.contains(&name) {
+                    Some(i)
+                } else {
+                    None
                 }
-                Some(i)
             })
             .next()
-    }
-}
-
-pub enum ResolverDiag<'s> {
-    RecursiveVarDecl {
-        name: Symbol<'s>,
-        decl: Span,
-        reference: Span,
-    },
-}
-
-impl Diagnostic for ResolverDiag<'_> {
-    fn into_diag(self) -> Diag {
-        match self {
-            ResolverDiag::RecursiveVarDecl {
-                name,
-                decl,
-                reference,
-            } => Diag::new(
-                DiagKind::Error,
-                format!("recursive definition of variable `{name}`"),
-            )
-            .with_primary(
-                reference,
-                format!("`{name}` referenced recursively in its own definition"),
-            )
-            .with_secondary(decl, format!("declaration of `{name}` here"))
-            .with_note("variable initializers cannot reference the variable being defined"),
-        }
     }
 }
